ca06600df44ad1468a71cc2fd6b6a3229af0d1a9
DHUSHYU <152497561+DHUSHYU@users.noreply.github.com>
2024-04-20 20:26:57 +0530

    Update Navbars.jsx

--- src/components/Navbars.jsx
+++ src/components/Navbars.jsx
@@ -21,9 +21,9 @@ const Navbars = () => {
     <>
        
       {/* Navbar */}
-      <nav id="lapview" className="z-10 sticky top-0 flex justify-between border rounded-r-lg border-gray-300 transition duration-500 ease-in-out hover:border-sky-500 p-4 bg-gray-800 text-white p-3">
+       <nav id="lapview" className="z-10  sticky top-0 flex justify-between border-4 rounded-r-lg hover:border-purple-400 transition duration-500 bg-black ease-in-out shado  p-2  text-white  ">
         <img
-          className=""
+          className="bg-purple-900  rounded-lg p-1 "
           src={logo}
           alt="Profile Image"
           width="80"
@@ -31,24 +31,24 @@ const Navbars = () => {
         />
         <div className="px-4 py-6 flex justify-end w-full">
           {/* Nav Links */}
-          <ul className="hidden sm:flex px-4 space-x-12">
+          <ul className="hidden sm:flex px-4 space-x-24">
             <li>
-              <Link to="/" className="text-white hover:bg-gray-400 font-bold hover:text-black px-3 py-3 rounded-md">
+              <Link to="/" className=" text-white shadow  text-indigo-700 hover:bg-purple-100 hover:text-purple-800    rounded-md font-bold  justify-center px-2 py-2">
                 Home
               </Link>
             </li>
             <li>
-              <Link to="/about" className="hover:text-black text-white hover:bg-gray-400 px-3 py-3 rounded-md font-bold">
+              <Link to="/about" className="text-white shadow  text-indigo-700 hover:bg-purple-100 hover:text-purple-800    rounded-md font-bold  justify-center px-2 py-2">
                 About
               </Link>
             </li>
             <li>
-              <Link to="/projects" className="hover:text-black text-white hover:bg-gray-400 px-3 py-3 rounded-md font-bold">
+              <Link to="/projects" className="text-white shadow  text-indigo-700 hover:bg-purple-100 hover:text-purple-800    rounded-md font-bold  justify-center px-2 py-2">
                 Projects
               </Link>
             </li>
             <li>
-              <Link to="/contactme" className="hover:text-black text-white hover:bg-gray-400 px-3 py-3 rounded-md font-bold">
+              <Link to="/contactme" className="text-white shadow  text-indigo-700 hover:bg-purple-100 hover:text-purple-800    rounded-md font-bold  justify-center px-2 py-2">
                 Contact
               </Link>
             </li>
