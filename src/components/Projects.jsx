f3c2d07b587b3a13a273a54170bbfdf046d0685b
DHUSHYU <smartworkdhushyu@gmail.com>
2024-04-27 23:03:38 +0530

    updated  project carousel

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,13 +1,14 @@
-import react, { useEffect } from 'react';
-import Biodata from "../assets/biodata.webp"
-import ParallaxVid from '../assets/parallex.webm'
+import React, { useEffect } from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import Biodata from "../assets/biodata.webp";
+import ParallaxVid from '../assets/parallex.webm';
 import NetflixVid from '../assets/Netflix.mp4';
-
 
 const Projects = () => {
   useEffect(() => {
     const sectionElement = document.getElementById("custom-animated-section");
-
     const options = {
       root: null,
       rootMargin: "0px",
@@ -32,13 +33,22 @@ const Projects = () => {
     };
   }, []);
 
-  return (
-    <>
-      <section id="projects">
-        <div className="container">
-          <div className="project-wrapper">
-            <h2 className="section-title dark-blue-text">Projects</h2>
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 10000 
+  };
 
+  return (
+    <section id="projects">
+      <div className="container">
+        <div className="project-wrapper">
+          <h2 className="section-title dark-blue-text">Projects</h2>
+          <Slider {...settings}>
             {/* Project 1 */}
             <div className="row">
               <div className="project-wrapper__text">
@@ -50,7 +60,6 @@ const Projects = () => {
                   <a href="https://github.com/DHUSHYANDAN/BioDATA.git" target="_blank" rel="noopener noreferrer" className="cta-btn text-color-main cta-btn--hero cta-btn--projects">Source Code</a>
                 </div>
               </div>
-
               <div className="project-wrapper__image">
                 <a href="https://dhushyandan.github.io/BioDATA/" target="_blank" rel="noopener noreferrer">
                   <div className="thumbnail rounded flex justify-center " id="custom-animated-section">
@@ -61,17 +70,16 @@ const Projects = () => {
             </div>
 
             {/* Project 2 */}
-            <article className="row">
+            <div className="row">
               <div className="project-wrapper__text">
                 <h3 className="project-wrapper__text-title">Parallax Website</h3>
                 <p className="project-wrapper__text-info" id="three">
-                  It is a simple level project. A typical website with a parallax effect is one in which the background of the website moves slower than the elements in the foreground. The visual effect we`&apos;`re able to achieve gives the impression of movement and depth as if there is space between the foreground and background.
+                  It is a simple level project. A typical website with a parallax effect is one in which the background of the website moves slower than the elements in the foreground. The visual effect we&apos;re able to achieve gives the impression of movement and depth as if there is space between the foreground and background.
                 </p>
                 <div className="project-wrapper__text-btns">
                   <a href="https://github.com/DHUSHYANDAN/parallax.git" target="_blank" rel="noopener noreferrer" className="cta-btn text-color-main cta-btn--hero cta-btn--projects">Source Code</a>
                 </div>
               </div>
-
               <div className="project-wrapper__image">
                 <a href="https://dhushyandan.github.io/parallax/" target="_blank" rel="noopener noreferrer">
                   <div className="thumbnail rounded">
@@ -82,10 +90,10 @@ const Projects = () => {
                   </div>
                 </a>
               </div>
-            </article>
+            </div>
 
             {/* Project 3 */}
-            <article className="row">
+            <div className="row">
               <div className="project-wrapper__text">
                 <h3 className="project-wrapper__text-title">Netflix Project</h3>
                 <p className="project-wrapper__text-info" id="four">
@@ -95,7 +103,6 @@ const Projects = () => {
                   <a href="https://github.com/DHUSHYANDAN/Netflix_model_project.git" target="_blank" rel="noopener noreferrer" className="cta-btn text-color-main cta-btn--hero cta-btn--projects">Source Code</a>
                 </div>
               </div>
-
               <div className="project-wrapper__image">
                 <a href=" https://dhushyandan.github.io/Netflix_model_project/" target="_blank" rel="noopener noreferrer">
                   <div className="thumbnail rounded">
@@ -106,12 +113,12 @@ const Projects = () => {
                   </div>
                 </a>
               </div>
-            </article>
-          </div>
+            </div>
+          </Slider>
         </div>
-      </section>
-    </>
+      </div>
+    </section>
   );
 };
 
-export default Projects
+export default Projects;
