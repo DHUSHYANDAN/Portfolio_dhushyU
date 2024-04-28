1a25bf1f78cc8c129501cb020ba65326ee3414d3
DHUSHYU <smartworkdhushyu@gmail.com>
2024-04-28 10:35:58 +0530

    updated  project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -40,29 +40,30 @@ const Projects = () => {
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 10000 
+    autoplaySpeed: 10000 ,
+    className: "bg-gray-900 text-gray-100 rounded-lg"
   };
 
   return (
     <section id="projects">
-      <div className="container">
-        <div className="project-wrapper">
+      <div className="container ">
+        <div className="project-wrapper ">
           <h2 className="section-title dark-blue-text">Projects</h2>
           <Slider {...settings}>
             {/* Project 1 */}
-            <div className="row">
-              <div className="project-wrapper__text">
+            <div className="row ">
+              <div className="project-wrapper__text p-8  ">
                 <h3 className="project-wrapper__text-title">Bio Data</h3>
-                <p className="project-wrapper__text-info" id="two">
+                <p className="project-wrapper__text-info " id="two ">
                   It is a basic level project. Also known as biographical data, a biodata typically includes a range of specific factual information about an individual. "It is a validation form". Essentially, biodata highlights a number of details about a person such as; name, age, color, height, skills, hobbies, allergies etc.
                 </p>
                 <div className="project-wrapper__text-btns">
                   <a href="https://github.com/DHUSHYANDAN/BioDATA.git" target="_blank" rel="noopener noreferrer" className="cta-btn text-color-main cta-btn--hero cta-btn--projects">Source Code</a>
-                </div>
+                </div><br /><br />
               </div>
-              <div className="project-wrapper__image">
+              <div className="project-wrapper__image  ">
                 <a href="https://dhushyandan.github.io/BioDATA/" target="_blank" rel="noopener noreferrer">
-                  <div className="thumbnail rounded flex justify-center " id="custom-animated-section">
+                  <div className="thumbnail rounded flex justify-center bg-gray-300 " id="custom-animated-section">
                     <img src={Biodata} className=" transform  transition-transform duration-1000" alt="Project Image" />
                   </div>
                 </a>
@@ -71,7 +72,7 @@ const Projects = () => {
 
             {/* Project 2 */}
             <div className="row">
-              <div className="project-wrapper__text">
+              <div className="project-wrapper__text p-8">
                 <h3 className="project-wrapper__text-title">Parallax Website</h3>
                 <p className="project-wrapper__text-info" id="three">
                   It is a simple level project. A typical website with a parallax effect is one in which the background of the website moves slower than the elements in the foreground. The visual effect we&apos;re able to achieve gives the impression of movement and depth as if there is space between the foreground and background.
@@ -79,7 +80,7 @@ const Projects = () => {
                 <div className="project-wrapper__text-btns">
                   <a href="https://github.com/DHUSHYANDAN/parallax.git" target="_blank" rel="noopener noreferrer" className="cta-btn text-color-main cta-btn--hero cta-btn--projects">Source Code</a>
                 </div>
-              </div>
+              </div><br /><br />
               <div className="project-wrapper__image">
                 <a href="https://dhushyandan.github.io/parallax/" target="_blank" rel="noopener noreferrer">
                   <div className="thumbnail rounded">
@@ -94,7 +95,7 @@ const Projects = () => {
 
             {/* Project 3 */}
             <div className="row">
-              <div className="project-wrapper__text">
+              <div className="project-wrapper__text p-8">
                 <h3 className="project-wrapper__text-title">Netflix Project</h3>
                 <p className="project-wrapper__text-info" id="four">
                   This project is a standard website featuring a home page and a sign-in page, similar to the layout of the Netflix website. It comprises four pages in total. The website is built using HTML and CSS.
@@ -102,7 +103,7 @@ const Projects = () => {
                 <div className="project-wrapper__text-btns">
                   <a href="https://github.com/DHUSHYANDAN/Netflix_model_project.git" target="_blank" rel="noopener noreferrer" className="cta-btn text-color-main cta-btn--hero cta-btn--projects">Source Code</a>
                 </div>
-              </div>
+              </div><br /><br />
               <div className="project-wrapper__image">
                 <a href=" https://dhushyandan.github.io/Netflix_model_project/" target="_blank" rel="noopener noreferrer">
                   <div className="thumbnail rounded">
@@ -117,6 +118,7 @@ const Projects = () => {
           </Slider>
         </div>
       </div>
+      
     </section>
   );
 };
